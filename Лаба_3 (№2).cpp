54b4ad8c8ec5e4bec3ce7ac8e36f25f2b3c9efa3
7roman0vna7 <90880098+7roman0vna7@users.noreply.github.com>
2021-10-22 02:12:44 +0300

    Add files via upload

---
+++ Лаба_3 (№2).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+long double factorial(int x) {         //функция для вычисления факториала
+	if (x < 0)                         
+		return 0;
+	if (x == 0)
+		return 1;
+	else
+		return x * factorial(x - 1);
+}
+
+int main() {
+	setlocale(0, "");
+	cout << "Введите число: " << endl;
+	int x = 0;
+	cin >> x;
+	cout << "Выберите дейстие:\n\
+1.Найти факториал числа.\n\
+2.Вывести все натуральные предыдущие числа." << "\n";
+	int c = 0;
+	cin >> c;
+	switch (c) {
+	case 1:
+		cout << "Факториал для числа " << x << " = " << factorial(x) << endl;
+		break;
+	case 2:
+		x = x + 1;                //чтобы в цикле вывелось ещё число, которое ввёл пользователь
+		cout << "Предыдущие числа: " << endl;
+		while (x > 0) {
+			x = x - 1;
+			cout << x << endl;
+		}
+		break;
+	}
+	return 0;
+}
